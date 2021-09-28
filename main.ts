88958e2bdaedd21b0c2e7e02e958c07298b85cd5
gpaetzold <76201189+gpaetzold@users.noreply.github.com>
2021-09-28 16:43:26 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,28 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . . # . .
+        . # . # .
+        . # # # .
+        . # . # .
+        . # . # .
+        `)
 })
+input.onButtonPressed(Button.AB, function () {
+    basic.showLeds(`
+        . . # # .
+        . # . . .
+        . # . . .
+        . # . . .
+        . . # # .
+        `)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . # # . .
+        . # . # .
+        . # # . .
+        . # . # .
+        . # # . .
+        `)
+})
+basic.showIcon(IconNames.Happy)
